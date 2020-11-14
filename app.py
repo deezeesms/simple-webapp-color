803f931cc78a0a35f0d5e8ce70dea5d38f2cf468
deezeesms <deezeesms@gmail.com>
2020-11-14 15:43:40 -0500

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ color = os.environ.get('APP_COLOR') or random.choice(["red","green","blue","blue
 
 @app.route("/")
 def main():
-    #return 'Hello'
+    #return 'Hello 2'
     print(color)
     return render_template('hello.html', name=socket.gethostname(), color=color_codes[color])
 
